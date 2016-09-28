b21dc0efd0761922dc232a7b66f4ccf9551e0054
Eugenio Liguori <eugenioliguori@gexy.it>
2016-09-28 16:29:18 +0200

    Initial implements of config management and plugin system

--- EScheme/src/org/escheme/Escheme.java
+++ EScheme/src/org/escheme/Escheme.java
@@ -3,9 +3,25 @@ package org.escheme;
 import org.escheme.gui.frame.ESFramePrjScheme;
 import org.escheme.lib.gui.ESDM;
 
+import com.gexy.config.Config;
+import com.gexy.config.exception.ConfigFileContextNotFound;
+import com.gexy.config.exception.ConfigFileParameterTooManyContext;
+import com.gexy.config.exception.ConfigFilePathException;
+
 public class Escheme {
+	public static Config conf;
 	public static ESDM esdm;
 	public static void main(String[] args) {
+		//carico configurazione
+		conf = new Config();
+		try {
+			conf.readConfigFromXml("config.xml","escheme");
+	 	} catch (ConfigFilePathException | ConfigFileContextNotFound | ConfigFileParameterTooManyContext e) {
+	 		System.err.println("Fatal error: Is not possile load config from XML file ./config.xml");
+	 		System.exit(1);
+	 	}
+		
+		//carico gestore UI
 		esdm = new ESDM();
 		
 		ESFramePrjScheme frame = new ESFramePrjScheme();
